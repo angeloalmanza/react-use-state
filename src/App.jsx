0e3a9cbd3b90a27b3350113846059837abb870c0
angeloalmanza <angeloalmanza26@gmail.com>
2024-12-16 15:33:56 +0100

    feat: bottoni, card e stato di react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import languages from './data/data'
 import { useState } from 'react'
-
 
 function App() {
 
+  const [ActiveLanguage, setActiveLanguage] = useState(1);
+
+  const ActiveLanguageData = languages.find((curLanguage) => curLanguage.id === ActiveLanguage);
+
   return (
     <>
-      <h1>Hello world</h1>
+      <h1>Learn Web development</h1>
+      <div className="container">
+        {languages.map((curLanguage) => (
+          <button onClick={() => {setActiveLanguage(curLanguage.id)}} key={curLanguage.id} className='btn btn-primary me-2 mb-2'>{curLanguage.title}</button>
+        ))}
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">{ActiveLanguageData.title}</h5>
+            <p className="card-text">{ActiveLanguageData.description}</p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
